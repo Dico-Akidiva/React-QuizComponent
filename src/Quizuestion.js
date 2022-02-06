69151450727f87580dfc74259235d8ad583b8df8
Mr.Robot <69214927+Dico-Akidiva@users.noreply.github.com>
2022-02-06 22:28:56 +0300

    module 3

---
+++ src/Quizuestion.js
@@ -0,0 +1,23 @@
+import React, {Component} from react
+
+class QuizQuestion extends Component {
+  render () {
+    <main>
+     <section>
+       <p>// instruction text goes here</p>
+     </section>
+     <section className="buttons">
+       <ul>
+         //quiz question button logic goes here
+       </ul>
+     </section>
+   </main>
+  }
+  constructor(quiz_question) {
+    super(quiz_question) {
+      
+    }
+  }
+}
+
+export default QuizQuestion
