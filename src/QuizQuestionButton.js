46aeba854779938ff6f377b8c63bb16066f13bf6
Mr.Robot <69214927+Dico-Akidiva@users.noreply.github.com>
2022-02-07 20:38:58 +0300

    right

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,17 +3,9 @@ import React, { Component} from  'react'
 
 class QuizQuestionButton extends Component {
   render() {
-    return (
-      <ul>
-      <li>
-      <button>{this.props.button_text}</button>
-      </li>
-      </ul>
+    return ()
+      <li><button>{this.props.button_text}</button></li>
     )
-  }
-  constructor(props) {
-    super(props)
-    this.props.quiz_question.answer_options[0]
   }
 }
 
