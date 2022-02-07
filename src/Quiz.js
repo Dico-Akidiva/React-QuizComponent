ce4b6f48cb21739d8c902d7828df950bf62bc972
Mr.Robot <69214927+Dico-Akidiva@users.noreply.github.com>
2022-02-07 21:39:51 +0300

    up

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,9 +10,14 @@ class Quiz extends Component {
       this.state = {quiz_position: 1}
   }
   render () {
+    const isQuizEnd = ((this.state.quiz_question - 1) ===
+  quizData.Quiz_questions.length)
     return (
-      
-      <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>
+
+      <div>
+      {isQuizEnd ? <QuizEnd /> :
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+      </div> }
     )
   }
 }
